c304d6ea6738504f4c8156e6a94407991134138f
Chathurika <chathurika-jayakody.com>
2023-03-01 15:40:18 -0500

    Add styles to lyrics

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -1,20 +1,23 @@
 import Image from "next/image"
 
-const Lyrics = ({lyrics, artist, title, image, date }) => {
+const Lyrics = ({ lyrics, artist, title, image, date }) => {
   return (
-    <div>
-        <h2>{title}</h2>
-        <p>{artist}</p>
-        <p>{date}</p>
+    <div className="mb-10">
+      <div className="flex flex-col items-center pb-5 pt-5 rounded-lg">
+        <h2 className="text-xl text-[#121640] font-bold">{title}</h2>
+        <p>Artist: {artist}</p>
+        <p className="mb-3">Release Date: {date}</p>
         <Image
-            src={image}
-            alt="image"
-            width={200}
-            height={200}
+          src={image}
+          alt="image"
+          width={200}
+          height={200}
+          className="rounded-md"
         >
 
         </Image>
-        <p className="css-fix">{lyrics}</p>
+      </div>
+      <p className="css-fix">{lyrics}</p>
     </div>
   )
 }
